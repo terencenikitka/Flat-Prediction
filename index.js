90460c44d24ffe00437e7cf8f246d542549fbee6
Felix Laniyan <flaniyanjr@gmail.com>
2023-09-25 16:15:00 -0400

    updated image html

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const predictionBtn = document.getElementById('getPrediction')
 let div = document.getElementById('innerDiv')
 let p = document.createElement('p')
 console.log(div)
-predictionBtn.addEventListener('click', ((e)=>{
+const pic= document.querySelector('div img')
+console.log(pic)
+
+
+
+fetch('http://localhost:3000/predictions')
+.then(r => r.json())
+.then(predictionsArray => {
+  predictionBtn.addEventListener('click', ((e)=>{
     p.innerText = 'today\'s prediction is ...'
-  div.append(p)
-setTimeout(()=>{alert('there is no fork')},0)
-}))
+    div.append(p)
+    const theChosenPrediction= predictionsArray[(Math.floor(Math.random() * predictionsArray.length))];
+    setTimeout(()=>{
+      alert(theChosenPrediction.prediction)
+      }, 0)
+      pic.src= theChosenPrediction.image
+  }))
+})
