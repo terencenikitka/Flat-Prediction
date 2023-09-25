958ea909911d2a3358be2f2267037cff4959d766
Nikita <terencekity@gmail.com>
2023-09-25 16:11:55 -0400

    get random selector

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const predictionBtn = document.getElementById('getPrediction')
 let div = document.getElementById('innerDiv')
 let p = document.createElement('p')
 console.log(div)
+let predictionText = '' 
+const img = document.getElementById('img')
+
+
 predictionBtn.addEventListener('click', ((e)=>{
     p.innerText = 'today\'s prediction is ...'
   div.append(p)
-setTimeout(()=>{alert('there is no fork')},0)
+
+
+fetch('http://127.0.0.1:3000/predictions')
+.then((p)=>p.json())
+.then((arr)=>{
+
+let randomId = Math.floor(arr.length*Math.random())+1
+
+arr.forEach(e => {
+  if (e.id === randomId){predictionText = e.prediction
+  img.src = e.image}
+  console.log(randomId)
+});
+
+})
+
+
+setTimeout(()=>{alert(predictionText)},350)
 }))
+
