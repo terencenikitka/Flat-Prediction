bf5cdd9f87f622b4dbad1a7f7d8aa8a4fcb6c610
Nikita <terencekity@gmail.com>
2023-09-25 17:02:36 -0400

    pulled css

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ let p = document.createElement('p')
 console.log(div)
 let predictionText = '' 
 const img = document.getElementById('img')
+// const nameInput = document.querySelector('form').name
+// const imgInput = document.querySelector('form').image
+const url = 'http://127.0.0.1:3000/predictions'
+// creating form event
+const form = document.querySelector('form')
+form.addEventListener('submit',(e)=>{
+e.preventDefault()
 
 
+
+console.log(e.target.name.value)
+})
 predictionBtn.addEventListener('click', ((e)=>{
     p.innerText = 'today\'s prediction is ...'
   div.append(p)
 
 
-fetch('http://127.0.0.1:3000/predictions')
+fetch(url)
 .then((p)=>p.json())
 .then((arr)=>{
 
