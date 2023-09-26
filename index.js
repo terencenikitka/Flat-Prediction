3662ace1e35d54f99f82c5e7d510ecfa32ac8b07
Nikita <terencekity@gmail.com>
2023-09-26 11:39:59 -0400

    add like and delete buton with a POST and DELETE request

--- index.js
+++ index.js
@@ -1,41 +1,75 @@
 const predictionBtn = document.getElementById('getPrediction')
 let div = document.getElementById('innerDiv')
 let p = document.createElement('p')
+const deletBtn = document.getElementById('delete button')
+const likeBtn = document.getElementById('like button')
 console.log(div)
-let predictionText = '' 
-const img = document.getElementById('img')
-// const nameInput = document.querySelector('form').name
-// const imgInput = document.querySelector('form').image
-const url = 'http://127.0.0.1:3000/predictions'
-// creating form event
-const form = document.querySelector('form')
-form.addEventListener('submit',(e)=>{
-e.preventDefault()
+cacheLike = {}
+let theChosenPrediction
+const pic= document.querySelector('#img')
+console.log(likeBtn)
 
 
 
-console.log(e.target.name.value)
-})
-predictionBtn.addEventListener('click', ((e)=>{
+fetch('http://localhost:3000/predictions')
+.then(r => r.json())
+.then(predictionsArray => {
+  /// display prediction button event
+  predictionBtn.addEventListener('click', ((e)=>{
     p.innerText = 'today\'s prediction is ...'
-  div.append(p)
+    div.append(p)
+     theChosenPrediction= predictionsArray[(Math.floor(Math.random() * predictionsArray.length))];
+    setTimeout(()=>{
+      alert(theChosenPrediction.prediction)
+      }, 300)
+      pic.src= theChosenPrediction.image
+      pic.style.display='';
+deletBtn.style.display = ''
+likeBtn.style.display = ''
+likeBtn.innerHTML = 'Like'
+likeBtn.disabled = false
+      //// delete button event
+deletBtn.addEventListener('click',((e)=>{
+deletePrediction(theChosenPrediction.id)
+ pic.style.display='none';
+console.log(theChosenPrediction.id)
 
-
-fetch(url)
-.then((p)=>p.json())
-.then((arr)=>{
-
-let randomId = Math.floor(arr.length*Math.random())+1
-
-arr.forEach(e => {
-  if (e.id === randomId){predictionText = e.prediction
-  img.src = e.image}
-  console.log(randomId)
-});
-
-})
-
-
-setTimeout(()=>{alert(predictionText)},350)
 }))
 
+// like button event 
+
+
+  }))
+likeBtn.addEventListener('click',((e)=>{
+  console.log('hey')
+  // console.log(likesTime)
+  likeBtn.innerHTML =` You liked that prediction ${++theChosenPrediction.likes} times`
+  // likeBtn.disabled = true
+  likeFunc(theChosenPrediction)
+}))
+})
+
+//delete btn function 
+function deletePrediction(id){
+  fetch(`http://localhost:3000/predictions/${id}`,{
+    method:'DELETE',
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+}
+
+/// like Function 
+
+function likeFunc (prediction){
+  
+
+fetch (`http://localhost:3000/predictions/${prediction.id}`,{
+  method: "PATCH",
+  headers:{
+    'Content-Type':'application/json'
+  },
+  body:JSON.stringify(prediction)
+})
+}
