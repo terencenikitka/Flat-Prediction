d8e6c6843d795386275da0440bf396560898f66b
Felix Laniyan <flaniyanjr@gmail.com>
2023-09-27 11:29:30 -0400

    polish branch merged into main branch

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const pic= document.querySelector('#img')
 const form= document.querySelector('form')
 
 
-
-
 fetch('http://localhost:3000/predictions')
 .then(r => r.json())
 .then(predictionsArray => {
@@ -17,64 +15,60 @@ fetch('http://localhost:3000/predictions')
   predictionBtn.addEventListener('click', ((e)=>{
     p.innerText = 'today\'s prediction is ...'
     div.append(p)
-     theChosenPrediction= predictionsArray[(Math.floor(Math.random() * predictionsArray.length))];
+    theChosenPrediction= predictionsArray[(Math.floor(Math.random() * predictionsArray.length))];
     setTimeout(()=>{
       alert(theChosenPrediction.prediction)
       }, 300)
-      pic.src= theChosenPrediction.image
-      pic.style.display='';
-deletBtn.style.display = ''
-likeBtn.style.display = ''
-likeBtn.innerHTML = 'Like'
-likeBtn.disabled = false
-      //// delete button event
-deletBtn.addEventListener('click',((e)=>{
-deletePrediction(theChosenPrediction.id)
- pic.style.display='none';
-console.log(theChosenPrediction.id)
-predictionsArray.forEach((el)=>{if(el.id===theChosenPrediction.id)predictionsArray.splice( (el.id) - 1,el.id)})
-likeBtn.style.display='none'
-deletBtn.style.display = 'none'
-}))
+    pic.src= theChosenPrediction.image
+    pic.style.display='';
+    deletBtn.style.display = ''
+    likeBtn.style.display = ''
+    likeBtn.innerHTML = 'Like'
+    likeBtn.disabled = false
+  //// delete button event
+  deletBtn.addEventListener('click',((e)=>{
+    deletePrediction(theChosenPrediction.id)
+    pic.style.display='none';
+    predictionsArray.forEach((el)=>{if(el.id===theChosenPrediction.id)predictionsArray.splice( (el.id) - 1,el.id)})
+    likeBtn.style.display='none'
+    deletBtn.style.display = 'none'
+  }))
 
 
-// img eventListenr
-pic.addEventListener('mouseenter', (e)=>{
-  setTimeout(()=>{
+  // img eventListenr
+  pic.addEventListener('mouseenter', (e)=>{
+    setTimeout(()=>{
     pic.style.display='none'
-deletBtn.style.display='none'
-likeBtn.style.display='none'},
-500)
-})
-pic.addEventListener('mouseout',((e)=>{
-  setTimeout (()=>{pic.style.display=''
-deletBtn.style.display=''
-likeBtn.style.display=''},510)
-}))
+    deletBtn.style.display='none'
+    likeBtn.style.display='none'},
+    500)
+  })
+  pic.addEventListener('mouseout',((e)=>{
+    setTimeout (()=>{pic.style.display=''
+    deletBtn.style.display=''
+    likeBtn.style.display=''},510)
+  }))
 
-// like button event 
+  // like button event 
 
 
   }))
-likeBtn.addEventListener('click',((e)=>{
-  console.log('hey')
-  // console.log(likesTime)
-  likeBtn.innerHTML =` You liked that prediction ${++theChosenPrediction.likes} times`
-  // likeBtn.disabled = true
-  likeFunc(theChosenPrediction)
-}))
+  likeBtn.addEventListener('click',((e)=>{
+    likeBtn.innerHTML =` You liked that prediction ${++theChosenPrediction.likes} times`
+    likeFunc(theChosenPrediction)
+  }))
 
-// submit event
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  const newPrediction= {
-    prediction: e.target.name.value,
-    image: e.target.image.value,
-    likes: 0
-  }
-  submitNewPrediction(newPrediction, predictionsArray)
-  form.reset()
-})
+  // submit event
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const newPrediction= {
+      prediction: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    submitNewPrediction(newPrediction, predictionsArray)
+    form.reset()
+  })
 })
 
 //delete btn function 
@@ -91,15 +85,13 @@ function deletePrediction(id){
 /// like Function 
 
 function likeFunc (prediction){
-  
-
-fetch (`http://localhost:3000/predictions/${prediction.id}`,{
-  method: "PATCH",
-  headers:{
+  fetch (`http://localhost:3000/predictions/${prediction.id}`,{
+    method: "PATCH",
+    headers:{
     'Content-Type':'application/json'
-  },
-  body:JSON.stringify(prediction)
-})
+    },
+    body:JSON.stringify(prediction)
+  })
 }
 
 // submit function
